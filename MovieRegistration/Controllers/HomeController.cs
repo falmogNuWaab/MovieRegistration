7840f015da96c5441034b598f5bc83fb7f175c42
James Mitchell <JamesMitchell@Rocketmortgage.com>
2021-12-01 15:40:16 -0500

    Initial commit of my Movie Registration App

---
+++ MovieRegistration/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieRegistration.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult MovieForm()
+        {
+            return View();
+        }
+        public IActionResult Result(Movie m)
+        {
+            return View(m);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
